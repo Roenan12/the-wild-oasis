2dcb088086125114e25a080067cdc347db5d7a74
Roenan Halili <haliliroenan12@gmail.com>
2024-10-24 21:32:57 +0800

    feat(bookings-filter): API-side filtering on bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  // show only totalPrice > 5000
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
   const {
     isPending,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter], // add filter to refetch data upon change (like dependency array)
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isPending, error, bookings };
