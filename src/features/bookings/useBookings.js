d9c96fd5863dc8a017f6c7132202dd22b3af1837
Roenan Halili <haliliroenan12@gmail.com>
2024-10-24 21:55:01 +0800

    feat(bookings-sort): API-side sorting on bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -15,13 +15,18 @@ export function useBookings() {
   // show only totalPrice > 5000
   // { field: "totalPrice", value: 5000, method: "gte" };
 
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc"; // get url and set the first option as default value
+  const [field, direction] = sortByRaw.split("-"); //separate into two
+  const sortBy = { field, direction };
+
   const {
     isPending,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter], // add filter to refetch data upon change (like dependency array)
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy], // add filter & sortBy to refetch data upon change (like dependency array)
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isPending, error, bookings };
