f0f4f4033ed57b8f1b5a55dc9a3168a0679877b2
Roenan Halili <haliliroenan12@gmail.com>
2024-10-29 15:32:22 +0800

    feat(authorization): protecting routes

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -10,3 +10,16 @@ export async function login({ email, password }) {
 
   return data;
 }
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession(); // get the data from local storage
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser(); // fetch current user
+
+  console.log(data);
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+}
