b8cd9c4e795a2c406e21da42875a70849ea864fa
Roenan Halili <haliliroenan12@gmail.com>
2024-10-30 21:44:45 +0800

    refactor: add auto re-fetch for user data

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -7,9 +7,11 @@ export function useUpdateUser() {
 
   const { mutate: updateUser, isPending: isUpdating } = useMutation({
     mutationFn: updateCurrentUser,
-    onSuccess: () => {
+    onSuccess: ({ user }) => {
       toast.success("User account successfully updated");
-      queryClient.invalidateQueries({ queryKey: ["user"] });
+      queryClient.setQueryData(["user"], user); //immediately set/update the
+
+      // queryClient.invalidateQueries({ queryKey: ["user"] });
     },
     onError: (err) => {
       toast.error(err.message);
