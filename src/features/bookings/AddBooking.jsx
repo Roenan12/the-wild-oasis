dd436c3dba5909842538349b52a255ed8a95ea69
Roenan Halili <haliliroenan12@gmail.com>
2024-11-02 16:01:30 +0800

    chore: code clean up

--- src/features/bookings/AddBooking.jsx
+++ src/features/bookings/AddBooking.jsx
@@ -17,23 +17,4 @@ function AddBooking() {
   );
 }
 
-/*
-function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
-  return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
-  );
-}
-  */
-
 export default AddBooking;
